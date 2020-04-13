d55d4d5517b62d7e2e9185eebd3e6fbbed3e5d1f
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-04-13 12:34:53 +0300

    Imported the http moule from the module files to the services files so as to inject it to the services and be able to use it with the url github api.ALso defined the blueprint class in the user.ts and imported it to the service to use the blueprint

--- src/app/service/github.service.ts
+++ src/app/service/github.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import{User} from '../user'
+import { from } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
+  user:User;
+  defaultUser:string;
 url:string ='https://api.github.com/users'
-  constructor() { }
+
+  constructor(private http:HttpClient) {
+    this.user=new User ('', '', '', '', 0, 0, 0);
+
+   }
 }
